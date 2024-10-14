ff18eebbec9233a65ad068f37a55e887330c70ed
ghosteater1311 <147140341+ghosteater1311@users.noreply.github.com>
2024-10-14 07:57:24 +0000

    Python small game

---
+++ Python/randomNumberGame.py
@@ -0,0 +1,11 @@
+import os
+import random
+
+print(f"Type a number in range 1 to 1000")
+
+num = int(input())
+
+if num == random.randint(1, 1000) :
+    print(f"you right!")
+else:
+    print(f"you wrong")
