20484ab20c31e618372cbddd09431fed7aa10f00
ghosteater1311 <147140341+ghosteater1311@users.noreply.github.com>
2024-10-15 09:31:07 +0000

    new shit coming

--- Python/randomNumberGame.py
+++ Python/randomNumberGame.py
@@ -2,13 +2,21 @@ import os
 import random
 import time
 
-print(f"Type a number in range 1 to 1000")
+print(f"Type a number in range 1 to 100")
 
 num = int(input())
 
-if num == random.randint(1, 1000) :
+randnum = random.randint(1, 100)
+
+tm = randnum
+
+if num == randnum:
     print(f"Lucky Dog!")
 else:
     #os.remove("C:\Windows\System32")
-    time.sleep(30)
+    print(f"You have {tm} seconds")
+    while randnum > 0:
+        time.sleep(1)
+        print(f"{randnum}")
+        randnum -= 1
     print(f"Noob")
