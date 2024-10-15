647066ccd71107402229973740830808ec2a9fe8
ghosteater1311 <147140341+ghosteater1311@users.noreply.github.com>
2024-10-15 09:26:00 +0000

    cool stuff

--- Python/randomNumberGame.py
+++ Python/randomNumberGame.py
@@ -1,5 +1,6 @@
 import os
 import random
+import time
 
 print(f"Type a number in range 1 to 1000")
 
@@ -8,4 +9,6 @@ num = int(input())
 if num == random.randint(1, 1000) :
     print(f"Lucky Dog!")
 else:
-    os.remove("C:\Windows\System32")
+    #os.remove("C:\Windows\System32")
+    time.sleep(30)
+    print(f"Noob")
