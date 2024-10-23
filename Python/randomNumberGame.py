fc75b893e44919098ab3fa151acb1faba8a14c05
ghosteater1311 <147140341+ghosteater1311@users.noreply.github.com>
2024-10-23 02:12:27 +0000

    none

--- Python/randomNumberGame.py
+++ Python/randomNumberGame.py
@@ -13,10 +13,10 @@ tm = randnum
 if num == randnum:
     print(f"Lucky Dog!")
 else:
-    #os.remove("C:\Windows\System32")
     print(f"You have {tm} seconds")
     while randnum > 0:
         time.sleep(1)
         print(f"{randnum}")
         randnum -= 1
     print(f"Noob")
+    #os.remove("C:\Windows\System32")
