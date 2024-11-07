d92b04aa3177e8ef53b065adba947f67a7eb7c59
ghosteater1311 <147140341+ghosteater1311@users.noreply.github.com>
2024-11-07 07:33:02 +0000

    reCreate complier

---
+++ Python/string.py
@@ -0,0 +1,7 @@
+input = "Four score and seven years ago"
+
+for c in input:
+    if c.lower() in ['a', 'e', 'i', 'o', 'u']:
+        print(c)
+
+#print([c for c in input if c.lower() in ['a', 'e', 'i', 'o', 'u']])
--- codedraft/draft.cpp
+++ codedraft/draft.cpp
@@ -1,49 +1,16 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool visited[8];
-
-void BFS(int adjMatrix[8][8]) 
-{  
-    queue<int> q;               
-    visited[0] = true;
-    q.push(0);
-    while (!q.empty()) 
-    {
-        int node = q.front();
-        cout << node << " "; 
-        q.pop();
-        for (int i = 0; i < 8; i++) 
-        {
-            if (adjMatrix[node][i] == 1 && !visited[i]) 
-            {
-                visited[i] = true;
-                q.push(i);
-            }
-        }
-    }
-    cout << endl;
+int check(int n)
+{
+    return n > 0 ? 1 : 0;
 }
 
-int main() 
+int main()
 {
-    for (int i = 0; i < 8; i++)
-    {
-       visited[i] = false;
-    }
-    int adjMatrix[8][8] = {
-        {0, 1, 1, 1, 1, 0, 0, 0},  
-        {1, 0, 0, 0, 0, 0, 0, 0},  
-        {1, 0, 0, 1, 0, 0, 0, 0},  
-        {1, 0, 1, 0, 0, 1, 0, 1},  
-        {1, 0, 0, 1, 0, 0, 1, 0}, 
-        {0, 0, 0, 1, 0, 0, 0, 0},  
-        {0, 0, 0, 0, 1, 0, 0, 0},  
-        {0, 0, 0, 1, 0, 0, 0, 0}   
-    };
-    for (int i = 0; i < 8; i++)
-    {
-        if (visited[i] == false) BFS(adjMatrix);
-    }
+    int n;
+    scanf("%d", &n);
+    printf("%d", check(n));
     return 0;
 }
