b454f7bed4cc6039f5785acbe69aa0c881887ec3
Nguyễn Đức Anh <147140341+ghosteater1311@users.noreply.github.com>
2024-11-20 10:57:13 +0000

    practice

--- codedraft/draft.cpp
+++ codedraft/draft.cpp
@@ -1,16 +1,22 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-int check(int n)
-{
-    return n > 0 ? 1 : 0;
-}
+int main() {
+    double items[] = {500, 12.4, 94, 45, 3, 81, 1000.9, 85, 90, 1, 35};
+    double discount;
+    int index;
+    double ans[11];
+    double minus[11];
+    cin >> discount;
+    for (index = 0; index < 11; index++)
+    {
+        minus[index] = items[index] * (discount / 100);
+        ans[index] = items[index] - minus[index]; 
+    }
+    for (index = 0; index < 11; index++)
+    {
+        cout << ans[index] << " ";
+    }
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    printf("%d", check(n));
     return 0;
 }
