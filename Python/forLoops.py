1e0b34177b87ded46f04114c14d6421f1a37f333
Nguyễn Đức Anh <bihbh5688@gmail.com>
2025-04-01 11:02:39 +0700

    profile

--- Python/forLoops.py
+++
@@ -1,3 +0,0 @@
-for i in range(0, 101, 1):
-    if i % 7 == 0:
-        print(i)
--- Python/string.py
+++
@@ -1,7 +0,0 @@
-input = "Four score and seven years ago"
-
-for c in input:
-    if c.lower() in ['a', 'e', 'i', 'o', 'u']:
-        print(c)
-
-#print([c for c in input if c.lower() in ['a', 'e', 'i', 'o', 'u']])
